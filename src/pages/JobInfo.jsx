16d06bf44425fa320e6b4e1873cc9a90ec960d0a
bdedujobinfo <bdedujob.info@gmail.com>
2024-10-16 19:57:17 +0600

    data update

--- src/pages/JobInfo.jsx
+++ src/pages/JobInfo.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 export const JobInfo = () => {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
-    axios.get("/public/data.json").then((response) => {
+    axios.get("https://bdedujobinfo.vercel.app/data.json").then((response) => {
       console.log(response?.data);
       setPosts(response.data);
     });
